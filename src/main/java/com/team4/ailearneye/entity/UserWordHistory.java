10c1bfbfd1e945cfa5060bce7d41dc6809ec4625
Park JongHyuk <94343223+jhyuk316@users.noreply.github.com>
2023-08-27 01:47:42 +0900

    UserWordHistory

--- src/main/java/com/team4/ailearneye/entity/UserWordHistory.java
+++ src/main/java/com/team4/ailearneye/entity/UserWordHistory.java
@@ -1,5 +1,6 @@
 package com.team4.ailearneye.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,13 +10,15 @@ import lombok.experimental.SuperBuilder;
 @Entity
 @SuperBuilder
 @NoArgsConstructor
-public class UserWordHistory extends BaseEntity{
+public class UserWordHistory extends BaseEntity {
 
     private long literacyId;
     private long memberId;
-    private long wordId;
+    private String word;
     private long time;
     private long rewindCount;
 
+    @Column(length = 500)
+    private String text;
 
 }
