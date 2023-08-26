8aac2546c56145744a23327a1342649940c11989
Park JongHyuk <94343223+jhyuk316@users.noreply.github.com>
2023-08-26 18:54:31 +0900

    entity

--- src/main/java/com/team4/ailearneye/entity/UserWordHistory.java
+++ src/main/java/com/team4/ailearneye/entity/UserWordHistory.java
@@ -2,6 +2,7 @@ package com.team4.ailearneye.entity;
 
 public class UserWordHistory extends BaseEntity{
 
+    private long literacyId;
     private long userId;
     private long wordId;
     private long time;
