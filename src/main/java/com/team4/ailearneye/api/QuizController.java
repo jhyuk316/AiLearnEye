39627f612722e5ccb434d26026295ac92a53ea40
Park JongHyuk <94343223+jhyuk316@users.noreply.github.com>
2023-08-26 23:44:20 +0900

    getQuiz

--- src/main/java/com/team4/ailearneye/api/QuizController.java
+++ src/main/java/com/team4/ailearneye/api/QuizController.java
@@ -1,9 +1,28 @@
 package com.team4.ailearneye.api;
 
+import com.team4.ailearneye.service.QuizService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("quiz")
 public class QuizController {
+
+    private final QuizService quizService;
+
+    @GetMapping
+    public ResponseEntity<?> getQuiz(@RequestParam("literacyId") long literacyId) {
+
+
+        return ResponseEntity.ok().build();
+    }
+
+
 }
---
+++ src/main/java/com/team4/ailearneye/service/QuizService.java
@@ -0,0 +1,13 @@
+package com.team4.ailearneye.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuizService {
+
+
+    public String getQuiz(){
+
+        return  "";}
+
+}
