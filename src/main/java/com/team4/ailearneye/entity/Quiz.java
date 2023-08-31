9f28959f00578c181b6c8cc45e00884458f665f4
Park JongHyuk <94343223+jhyuk316@users.noreply.github.com>
2023-08-31 23:01:51 +0900

    add WordRepository

---
+++ src/main/java/com/team4/ailearneye/entity/Quiz.java
@@ -0,0 +1,17 @@
+package com.team4.ailearneye.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+public class Quiz extends BaseEntity {
+
+    private long paragraphId;
+    private String title;
+    
+}
