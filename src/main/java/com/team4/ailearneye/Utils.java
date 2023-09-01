b3a510ff30c6fb1688914c6f7b1d5ed8a2c9445c
Park JongHyuk <94343223+jhyuk316@users.noreply.github.com>
2023-09-01 21:31:10 +0900

    add tojson

--- src/main/java/com/team4/ailearneye/Utils.java
+++ src/main/java/com/team4/ailearneye/Utils.java
@@ -1,5 +1,7 @@
 package com.team4.ailearneye;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import lombok.extern.slf4j.Slf4j;
@@ -10,9 +12,17 @@ public class Utils {
     public static final Gson gson = new Gson();
     private static final Gson gsonPretty = new GsonBuilder().setPrettyPrinting().create();
 
-    // public static String toJson(Object object) {
-    //     return gson.toJson(object);
-    // }
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String toJson(Object object) {
+        String json = "";
+        try {
+            json = objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            log.info(e.getMessage(), e);
+        }
+        return json;
+    }
     //
     // public static String toJsonPretty(Object object) {
     //     return gsonPretty.toJson(object);
